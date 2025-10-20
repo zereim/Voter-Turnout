e3fea6fe208c83dab1d319586e35ebac01b69589
zereim <wtruskolaski@gmail.com>
2025-10-20 12:12:06 -0400

    Added another dataset (MIT), and loaded it into the program

--- Voter_Turnout_main.R
+++ Voter_Turnout_main.R
@@ -1,9 +1,24 @@
 library(tidyverse)
 library(readr)
+library(ggplot2)
 
 # load the voter csv file 
-NC_Voter_convert <- read_csv("NC_Voter_convert.csv")
+NC_Voter_convert <- read_csv('NC_Voter_convert.csv')
+
+# load the MIT voter csv file
+MIT_NC_Voter <- read_csv('MIT NC-cleaned-final.csv')
 
 # give a glimpse 
 head(NC_Voter_convert)
+head(MIT_NC_Voter)
+     
+
+# Tie the Vote Share to shapefiles 
+
+
+
+# Plot the data in a chloropleth map
+
+
+
 
