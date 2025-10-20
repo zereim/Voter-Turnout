1e663ea5d10d39fbd1a22d953ec74d54e85cae8f
zereim <wtruskolaski@gmail.com>
2025-10-20 19:59:56 -0400

    Created a variable containing all the presidential elections.

--- Voter_Turnout_main.R
+++ Voter_Turnout_main.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(readr)
-library(ggplot2)
+library(stringr)
+
 
 # load the voter csv file 
 NC_Voter_convert <- read_csv('NC_Voter_convert.csv')
@@ -8,14 +9,21 @@ NC_Voter_convert <- read_csv('NC_Voter_convert.csv')
 # load the MIT voter csv file
 MIT_NC_Voter <- read_csv('MIT NC-cleaned-final.csv')
 
+
+MIT_NC_Slice <- MIT_NC_Voter %>% 
+  filter(str_detect(office, 'US PRESIDENT'))
+
+
+
 # give a glimpse 
-head(NC_Voter_convert)
-head(MIT_NC_Voter)
+#head(NC_Voter_convert)
+#head(MIT_NC_Voter)
      
 
 # Tie the Vote Share to shapefiles 
 
-
+# 
+# jurisdiction_fips
 
 # Plot the data in a chloropleth map
 
