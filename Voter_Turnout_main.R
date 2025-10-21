631569e0316db8204407c1511522fffa0acf39b6
zereim <wtruskolaski@gmail.com>
2025-10-20 22:49:18 -0400

    added janitor, cleaned Pres dataframe, created new dataframe to hold pres vote totals

--- Voter_Turnout_main.R
+++ Voter_Turnout_main.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(readr)
 library(stringr)
+library(janitor)
 
 
 # load the voter csv file 
@@ -9,11 +10,19 @@ NC_Voter_convert <- read_csv('NC_Voter_convert.csv')
 # load the MIT voter csv file
 MIT_NC_Voter <- read_csv('MIT NC-cleaned-final.csv')
 
-
+# selecting presidential election data
 MIT_NC_Pres <- MIT_NC_Voter %>% 
   filter(str_detect(office, 'US PRESIDENT'))
 
+# cleaning data
+MIT_NC_Pres_Clean <- clean_names(MIT_NC_Pres)
 
+# Creating a new dataframe that sums the different presidential candidates vote totals 
+Pres_Total <- MIT_NC_Pres_Clean %>%
+  # group all rows by candidate
+  group_by(candidate) %>%          
+  # sum votes per candidate
+  summarise(total_votes = sum(votes, na.rm = TRUE))  
 
 # give a glimpse 
 #head(NC_Voter_convert)
