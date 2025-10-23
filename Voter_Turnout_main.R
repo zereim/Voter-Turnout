fc08652020047f7a54c8947ee42f44002176bbba
zereim <wtruskolaski@gmail.com>
2025-10-23 00:27:50 -0400

    Added API key and dataframe for ACS. Pulled population stats for counties from it

--- Voter_Turnout_main.R
+++ Voter_Turnout_main.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(readr)
 library(stringr)
 library(janitor)
+library(tidycensus)
 
 
 # load the voter csv file 
@@ -24,6 +25,36 @@ Pres_Total <- MIT_NC_Pres_Clean %>%
   # sum votes per candidate
   summarise(total_votes = sum(votes, na.rm = TRUE))  
 
+
+census_api_key("090766422394c061e3e065a0d87f7cc5efed8d21", install = TRUE)
+
+# Example: Get 2020 county-level total population
+pop <- get_acs(
+  geography = "county",
+  variables = "B01003_001",   # total population
+  year = 2020,
+  survey = "acs5"
+)
+
+
+# API key: 090766422394c061e3e065a0d87f7cc5efed8d21
+
+
+
+
+#df <- read_delim(
+#  "ncvoter_Statewide.txt",      # your file path
+#  delim = "\t",                 # tab-delimited
+#  quote = "\"",                 # fields are quoted
+#  locale = locale(encoding = "windows-1252"),  # handle encoding
+#  show_col_types = FALSE        # suppress verbose column type message
+#)
+
+# Clean column names
+df <- clean_names(df)
+
+# View the structure
+glimpse(df)
 # give a glimpse 
 #head(NC_Voter_convert)
 #head(MIT_NC_Voter)
